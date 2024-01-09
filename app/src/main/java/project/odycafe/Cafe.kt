e60f183c53ae66e0ae4aaff31ad4e13e332e220b
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2024-01-09 11:17:15 +0700

    Add data class 'Menu' & 'Pesanan' with foreign key

--- app/src/main/java/project/odycafe/Cafe.kt
+++ app/src/main/java/project/odycafe/Cafe.kt
@@ -1,2 +1,42 @@
 package project.odycafe
 
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "tblMenu",
+    indices = [Index(value = ["menu"], unique = true)] // Menambahkan indeks unik pada kolom menu
+)
+data class Menu(
+    @PrimaryKey
+    val idmenu : Int,
+    val menu : String,
+    val harga : String,
+    val ketersediaan : String,
+    val kategori : String,
+)
+
+
+@Entity(
+    tableName = "tblPesanan",
+    foreignKeys = [ForeignKey(
+        // foreign key nya diambil dari data class Menu
+        entity = Menu::class,
+        parentColumns = ["menu"],
+        childColumns = ["idMenuForeignKey"],
+        onDelete = ForeignKey.CASCADE
+    )],
+    indices = [Index("idMenuForeignKey")] // Menambahkan index untuk idMenuForeignKey
+)
+
+data class Pesanan(
+    @PrimaryKey
+    val idpesanan : Int,
+    val nama : String,
+    val detail : String,
+    val metode : String,
+    val tanggal : String,
+    val idMenuForeignKey: String // Foreign key reference to idmenu from Menu
+)
