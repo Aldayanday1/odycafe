9e328c6b13cc6bcd6f49c688ade437598f8def1d
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2024-01-09 16:38:38 +0700

    Create Repositori (Menu & Pesanan)

---
+++ app/src/main/java/project/odycafe/repositori/RepositoriCafe.kt
@@ -0,0 +1,35 @@
+package project.odycafe.repositori
+
+import android.view.Menu
+import kotlinx.coroutines.flow.Flow
+import project.odycafe.data.Pesanan
+interface RepositoriMenu {
+    suspend fun insertMenu(menu: Menu)
+
+    suspend fun updateMenu(menu: Menu)
+
+    suspend fun deleteMenu(menu: Menu)
+
+    fun getMenuStream(id: Int): Flow<Menu?>
+
+    fun getAllMenuStream(): Flow<List<Menu>>
+
+    // Search Fiture
+    fun searchMenu(query: String): Flow<List<Menu>>
+    suspend fun insertSearchMenu(menu: Menu)
+
+    // Save URL Image to Database
+    suspend fun updateMenuPhoto(idmenu: Int, imageUrl: String)
+}
+
+interface RepositoriPesanan {
+
+    suspend fun insertPesanan(pesanan: Pesanan)
+
+    suspend fun updatePesanan(pesanan: Pesanan)
+
+    suspend fun deletePesanan(pesanan: Pesanan)
+
+    fun getPesananStream(id: Int): Flow<Pesanan?>
+    fun getAllPesananStream(): Flow<List<Pesanan>>
+}
