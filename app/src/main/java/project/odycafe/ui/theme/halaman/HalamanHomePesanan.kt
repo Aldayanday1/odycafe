f2232ea84ca2de1972d5d7234d9a5066fc71ec00
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2024-01-11 05:14:44 +0700

    Add Function of PesananScreen

--- app/src/main/java/project/odycafe/ui/theme/halaman/HalamanHomePesanan.kt
+++ app/src/main/java/project/odycafe/ui/theme/halaman/HalamanHomePesanan.kt
@@ -1,6 +1,12 @@
 package project.odycafe.ui.theme.halaman
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import project.odycafe.R
+import project.odycafe.model.HomeViewModel
+import project.odycafe.model.PenyediaViewModel
 import project.odycafe.navigasi.DestinasiNavigasi
 
 object DestinasiPesanan : DestinasiNavigasi {
@@ -8,3 +14,15 @@ object DestinasiPesanan : DestinasiNavigasi {
     override val titleRes = R.string.welcome_pesanan
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PesananScreen(
+    navigateToItemEntry: () -> Unit,
+    onDetailClick: (Int) -> Unit = {},
+    navigateBack: () -> Unit,
+    navigateToHome: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+
+}
