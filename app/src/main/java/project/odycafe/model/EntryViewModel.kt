632f6d0b56223e9a540d2b55bb1bfc121c64d134
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2024-01-11 01:30:29 +0700

    Add All Function Menu on EntryViewModel

--- app/src/main/java/project/odycafe/model/EntryViewModel.kt
+++ app/src/main/java/project/odycafe/model/EntryViewModel.kt
@@ -2,6 +2,7 @@ package project.odycafe.model
 
 
 import androidx.lifecycle.ViewModel
+import project.odycafe.data.Menu
 import project.odycafe.data.Pesanan
 import project.odycafe.repositori.RepositoriMenu
 import project.odycafe.repositori.RepositoriPesanan
@@ -13,6 +14,47 @@ class EntryViewModel(
 }
 
 
+
+/* ------------- MENU ------------ */
+
+// Mewakili Status Ui untuk Menu
+data class UIStateMenu(
+    val detailMenu: DetailMenu = DetailMenu(),
+    val isEntryValid: Boolean = false
+)
+data class DetailMenu(
+    val idmenu: Int? = null,
+    val menu: String = "",
+    val harga: String = "",
+    val ketersediaan: String = "",
+    val kategori: String = "",
+    val foto: String = "",
+)
+
+// Fungsi untuk mengkonversi data input ke data dalam tabel Menu
+fun DetailMenu.toMenu(): Menu = Menu(
+    idmenu = idmenu ?: 0,
+    menu = menu,
+    harga = harga,
+    ketersediaan = ketersediaan,
+    kategori = kategori,
+    foto = foto
+)
+// Fungsi untuk mengubah Menu menjadi UIStateMenu
+fun Menu.toUiStateMenu(isEntryValid: Boolean = false): UIStateMenu = UIStateMenu(
+    detailMenu = this.toDetailMenu(),
+    isEntryValid = isEntryValid
+)
+
+// Fungsi untuk mengubah Menu menjadi DetailMenu
+fun Menu.toDetailMenu(): DetailMenu = DetailMenu(
+    idmenu = idmenu,
+    menu = menu,
+    harga = harga,
+    ketersediaan = ketersediaan,
+    kategori = kategori,
+    foto = foto
+)
 
     /* ------------- PESANAN ------------ */
 
