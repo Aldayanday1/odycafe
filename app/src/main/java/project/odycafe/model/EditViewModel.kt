44af355617cc54f23a2d7350ddec9b5c8fcecd1a
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2024-01-11 02:09:57 +0700

    Add variabel menuUi and pesananUi

--- app/src/main/java/project/odycafe/model/EditViewModel.kt
+++ app/src/main/java/project/odycafe/model/EditViewModel.kt
@@ -1,6 +1,10 @@
 package project.odycafe.model
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
 import project.odycafe.repositori.RepositoriMenu
 import project.odycafe.repositori.RepositoriPesanan
 
@@ -9,4 +13,10 @@ class EditViewModel(
     private val repositoriMenu: RepositoriMenu,
     private val repositoriPesanan: RepositoriPesanan
 
-)
+) : ViewModel(){
+    var menuUiState by mutableStateOf(UIStateMenu())
+        private set
+    var pesananUiState by mutableStateOf(UIStatePesanan())
+        private set
+
+}
