2a32d587075cb1afd9dc0262c17e78f57ea6f528
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2024-01-11 02:13:22 +0700

    Add instance val menuitem and function to getAllMenu

--- app/src/main/java/project/odycafe/model/EditViewModel.kt
+++ app/src/main/java/project/odycafe/model/EditViewModel.kt
@@ -5,6 +5,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import project.odycafe.data.Menu
 import project.odycafe.repositori.RepositoriMenu
 import project.odycafe.repositori.RepositoriPesanan
 
@@ -19,4 +24,24 @@ class EditViewModel(
     var pesananUiState by mutableStateOf(UIStatePesanan())
         private set
 
+    /*--------DROPDOWN--------*/
+
+    // Properti untuk daftar menu
+    private val _menuItems = MutableStateFlow<List<Menu>>(emptyList())
+    val menuItems: StateFlow<List<Menu>> = _menuItems
+
+    init {
+        // Panggil fungsi untuk mendapatkan daftar menu dari repositori
+        getAllMenu()
+    }
+
+    // Fungsi untuk mendapatkan daftar menu dari repositori
+    private fun getAllMenu() {
+        viewModelScope.launch {
+            repositoriMenu.getAllMenuStream().collect { menuList ->
+                // Update daftar menu saat data berubah
+                _menuItems.value = menuList
+            }
+        }
+    }
 }
