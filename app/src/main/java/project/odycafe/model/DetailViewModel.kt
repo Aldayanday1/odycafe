e620d7eadf096b171249e2e960b8163a5642bad2
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2024-01-11 01:59:26 +0700

    Add Class DetailsViewModel

--- app/src/main/java/project/odycafe/model/DetailViewModel.kt
+++ app/src/main/java/project/odycafe/model/DetailViewModel.kt
@@ -1,2 +1,12 @@
 package project.odycafe.model
 
+import androidx.lifecycle.SavedStateHandle
+import project.odycafe.repositori.RepositoriMenu
+import project.odycafe.repositori.RepositoriPesanan
+
+class DetailsViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val repositoriMenu: RepositoriMenu,
+    private val repositoriPesanan: RepositoriPesanan
+
+)
