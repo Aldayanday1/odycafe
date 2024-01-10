385238f9db1e14fb636e353969cb008cc1275611
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2024-01-11 02:08:04 +0700

    Add class EditViewModel

--- app/src/main/java/project/odycafe/model/EditViewModel.kt
+++ app/src/main/java/project/odycafe/model/EditViewModel.kt
@@ -1,2 +1,12 @@
 package project.odycafe.model
 
+import androidx.lifecycle.SavedStateHandle
+import project.odycafe.repositori.RepositoriMenu
+import project.odycafe.repositori.RepositoriPesanan
+
+class EditViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoriMenu: RepositoriMenu,
+    private val repositoriPesanan: RepositoriPesanan
+
+)
