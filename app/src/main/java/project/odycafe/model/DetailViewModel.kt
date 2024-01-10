7eeb049d0c2bd57fc77cef7785c77b5314c868a0
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2024-01-11 02:04:53 +0700

    Add instance val menu and pesanan to get data

--- app/src/main/java/project/odycafe/model/DetailViewModel.kt
+++ app/src/main/java/project/odycafe/model/DetailViewModel.kt
@@ -1,6 +1,13 @@
 package project.odycafe.model
 
 import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import project.odycafe.repositori.RepositoriMenu
 import project.odycafe.repositori.RepositoriPesanan
 
@@ -9,7 +16,48 @@ class DetailsViewModel (
     private val repositoriMenu: RepositoriMenu,
     private val repositoriPesanan: RepositoriPesanan
 
-)
+): ViewModel(){
+
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+
+    private val detailId: Int = checkNotNull(savedStateHandle[DetailsMenuDestination.detailIdArg])
+
+    // get id Menu
+    val uiStateMenu: StateFlow<ItemDetailsMenuUiState> =
+        repositoriMenu.getMenuStream(detailId)
+            .filterNotNull()
+            .map {
+                ItemDetailsMenuUiState(detailMenu = it.toDetailMenu())
+            }.stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+                initialValue = ItemDetailsMenuUiState()
+            )
+
+    // get id Pesanan
+    val uiStatePesanan: StateFlow<ItemDetailsPesananUiState> =
+        repositoriPesanan.getPesananStream(detailId)
+            .filterNotNull()
+            .map {
+                ItemDetailsPesananUiState(detailPesanan = it.toDetailPesanan())
+            }.stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+                initialValue = ItemDetailsPesananUiState()
+            )
+
+    // delete Menu
+    suspend fun deleteMenuItem(){
+        repositoriMenu.deleteMenu(uiStateMenu.value.detailMenu.toMenu())
+    }
+
+    // delete Pesanan
+    suspend fun deletePesananItem(){
+        repositoriPesanan.deletePesanan(uiStatePesanan.value.detailPesanan.toPesanan())
+    }
+}
 
 data class ItemDetailsMenuUiState(
     val detailMenu: DetailMenu = DetailMenu(),
