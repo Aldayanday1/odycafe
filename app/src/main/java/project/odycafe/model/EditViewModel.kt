066ecc1db92a567ae3889dd26bcde7db0d06f829
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2024-01-11 02:31:42 +0700

    Add init to get repositori menu and pesanan

--- app/src/main/java/project/odycafe/model/EditViewModel.kt
+++ app/src/main/java/project/odycafe/model/EditViewModel.kt
@@ -8,6 +8,8 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import project.odycafe.data.Menu
 import project.odycafe.repositori.RepositoriMenu
@@ -23,6 +25,41 @@ class EditViewModel(
         private set
     var pesananUiState by mutableStateOf(UIStatePesanan())
         private set
+
+    private val editmenuId: Int = checkNotNull(savedStateHandle[ItemEditMenuDestination.editIdArg])
+    private val editpesananId: Int = checkNotNull(savedStateHandle[ItemEditPesananDestination.editIdArg])
+
+    // get (id)
+    init {
+        viewModelScope.launch {
+            menuUiState = repositoriMenu.getMenuStream(editmenuId)
+                .filterNotNull()
+                .first()
+                .toUiStateMenu(true)
+            pesananUiState = repositoriPesanan.getPesananStream(editpesananId)
+                .filterNotNull()
+                .first()
+                .toUiStatePesanan(true)
+        }
+    }
+
+    suspend fun updateMenu(){
+        if (validasiInputMenu(menuUiState.detailMenu)){
+            repositoriMenu.updateMenu(menuUiState.detailMenu.toMenu())
+        }
+        else {
+            println("Menu Data not valid")
+        }
+    }
+    suspend fun updatePesanan(menuItems: List<Menu>) {
+        if (validasiInputPesanan(pesananUiState.detailPesanan, menuItems)) {
+            repositoriPesanan.updatePesanan(pesananUiState.detailPesanan.toPesanan())
+        } else {
+            println("Pesanan Data not valid")
+        }
+    }
+
+
 
     fun updateUiStateMenu(detailMenu: DetailMenu){
         menuUiState = UIStateMenu(
