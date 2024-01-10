4cf2d938c5e21fe908188046f6885275fbd5472b
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2024-01-11 06:58:33 +0700

    Add functon of ItemEditMenuScreen

--- app/src/main/java/project/odycafe/ui/theme/halaman/HalamanEditMenu.kt
+++ app/src/main/java/project/odycafe/ui/theme/halaman/HalamanEditMenu.kt
@@ -1,6 +1,12 @@
 package project.odycafe.ui.theme.halaman
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import project.odycafe.R
+import project.odycafe.model.EditViewModel
+import project.odycafe.model.PenyediaViewModel
 import project.odycafe.navigasi.DestinasiNavigasi
 
 object ItemEditMenuDestination : DestinasiNavigasi {
@@ -8,4 +14,15 @@ object ItemEditMenuDestination : DestinasiNavigasi {
     override val titleRes = R.string.title_edit_menu
     const val editIdArg = "itemId"
     val routeWithArgs = "$route/{$editIdArg}"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ItemEditMenuScreen(
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+
 }
