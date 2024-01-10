83e3140d09c65438e51b75079adf1c71a0261cc4
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2024-01-11 03:31:20 +0700

    Add object DestinasiMenu for navigasi

--- app/src/main/java/project/odycafe/ui/theme/halaman/HalamanHomeMenu.kt
+++ app/src/main/java/project/odycafe/ui/theme/halaman/HalamanHomeMenu.kt
@@ -4,10 +4,15 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
+import project.odycafe.R
 import project.odycafe.model.HomeViewModel
 import project.odycafe.model.PenyediaViewModel
+import project.odycafe.navigasi.DestinasiNavigasi
 
-
+object DestinasiMenu : DestinasiNavigasi {
+    override val route = "menu"
+    override val titleRes = R.string.welcome_menu
+}
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MenuScreen(
