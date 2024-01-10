ad49affcc062a491a27abc475389afcc41f7aed3
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2024-01-11 05:13:40 +0700

    Add File 'Halaman Home Pesanan' with a object DestinasiPesanan

---
+++ app/src/main/java/project/odycafe/ui/theme/halaman/HalamanHomePesanan.kt
@@ -0,0 +1,10 @@
+package project.odycafe.ui.theme.halaman
+
+import project.odycafe.R
+import project.odycafe.navigasi.DestinasiNavigasi
+
+object DestinasiPesanan : DestinasiNavigasi {
+    override val route = "pesanan"
+    override val titleRes = R.string.welcome_pesanan
+}
+
