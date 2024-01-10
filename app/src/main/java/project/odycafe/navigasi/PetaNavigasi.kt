8b04670d70f81f27aa49d5906ce32d079b1c0aee
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2024-01-11 03:37:17 +0700

    Add functionAppBar on PetaNavigasi.kt

--- app/src/main/java/project/odycafe/navigasi/PetaNavigasi.kt
+++ app/src/main/java/project/odycafe/navigasi/PetaNavigasi.kt
@@ -1,2 +1,51 @@
 package project.odycafe.navigasi
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import project.odycafe.R
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CafeTopAppBar(
+    title: String,
+    canNavigateBack: Boolean,
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    navigateUp: () -> Unit = {}
+){
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = title,
+                fontFamily = FontFamily.Cursive,
+                fontWeight = FontWeight.ExtraBold
+            )
+        },
+        modifier = modifier,
+        scrollBehavior = scrollBehavior,
+        navigationIcon = {
+            if (canNavigateBack){
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = stringResource(R.string.back)
+                    )
+                }
+            }
+        }
+    )
+}
