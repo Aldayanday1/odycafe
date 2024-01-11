27ff2ad7452332abd78b90cfb736097a18e17739
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2024-01-11 07:22:05 +0700

    Add function of EntryMenuScreen

--- app/src/main/java/project/odycafe/ui/theme/halaman/HalamanEntryMenu.kt
+++ app/src/main/java/project/odycafe/ui/theme/halaman/HalamanEntryMenu.kt
@@ -1,7 +1,22 @@
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import project.odycafe.R
+import project.odycafe.model.EntryViewModel
+import project.odycafe.model.PenyediaViewModel
 import project.odycafe.navigasi.DestinasiNavigasi
 
 object DestinasiMenuEntry: DestinasiNavigasi {
     override val route = "item_entry_menu"
     override val titleRes = R.string.title_entry_menu
+}
+
+@Composable
+fun EntryMenuScreen(
+    onNavigateUp: () -> Unit,
+    navigateBack: () -> Unit,
+    modifier: Modifier,
+    viewModel: EntryViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+
 }
