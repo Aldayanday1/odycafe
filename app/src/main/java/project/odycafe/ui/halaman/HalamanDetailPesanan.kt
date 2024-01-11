69bb62c25b46a4b243b0887bdc5c40ab5734e126
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2024-01-11 14:21:27 +0700

    Add Function of DeletePesananConfirmationDialog

--- app/src/main/java/project/odycafe/ui/halaman/HalamanDetailPesanan.kt
+++ app/src/main/java/project/odycafe/ui/halaman/HalamanDetailPesanan.kt
@@ -1,5 +1,11 @@
 package project.odycafe.ui.halaman
 
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import project.odycafe.R
 import project.odycafe.navigasi.DestinasiNavigasi
 
@@ -10,3 +16,31 @@ object DetailsPesananDestination : DestinasiNavigasi {
     val routeWithArgs = "$route/{$detailIdArg}"
 }
 
+@Composable
+private fun DeletePesananConfirmationDialog(
+    onDeleteConfirm: () -> Unit,
+    onDeleteCancel: () -> Unit,
+    deleteConfirmationRequired: Boolean, // Gunakan parameter baru sebagai trigger
+    modifier: Modifier = Modifier
+){
+    if (deleteConfirmationRequired) { // Ubah penanganan tampilan dialog berdasarkan status
+        AlertDialog(
+            onDismissRequest = { /*TODO*/ },
+            title = { Text(stringResource(id = R.string.attention)) },
+            text = { Text(text = stringResource(id = R.string.delete)) },
+            modifier = modifier,
+            dismissButton = {
+                TextButton(onClick = {
+                    onDeleteCancel() // Panggil onDeleteCancel untuk mereset status
+                }) {
+                    Text(text = stringResource(id = R.string.no))
+                }
+            },
+            confirmButton = {
+                TextButton(onClick = onDeleteConfirm) {
+                    Text(text = stringResource(id = R.string.yes))
+                }
+            }
+        )
+    }
+}
