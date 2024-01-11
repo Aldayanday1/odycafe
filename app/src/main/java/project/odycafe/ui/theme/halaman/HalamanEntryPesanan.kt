7b9a3f1bc923ea8af99fb36cc6ada9f36ef32b33
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2024-01-11 13:26:40 +0700

    Add a New file with Object of DestinasiPesananEntry

---
+++ app/src/main/java/project/odycafe/ui/theme/halaman/HalamanEntryPesanan.kt
@@ -0,0 +1,9 @@
+package project.odycafe.ui.theme.halaman
+
+import project.odycafe.R
+import project.odycafe.navigasi.DestinasiNavigasi
+
+object DestinasiPesananEntry: DestinasiNavigasi {
+    override val route = "item_entry_pesanan"
+    override val titleRes = R.string.title_entry_pesanan
+}
