aee456efda27b9cdaa7d70a00eaa9eb9e7b9be68
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2024-01-11 07:02:55 +0700

    Add Scaffold for topBar in ItemEditMenuScreen

--- app/src/main/java/project/odycafe/ui/theme/halaman/HalamanEditMenu.kt
+++ app/src/main/java/project/odycafe/ui/theme/halaman/HalamanEditMenu.kt
@@ -1,12 +1,18 @@
 package project.odycafe.ui.theme.halaman
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
+import kotlinx.coroutines.launch
 import project.odycafe.R
 import project.odycafe.model.EditViewModel
 import project.odycafe.model.PenyediaViewModel
+import project.odycafe.navigasi.CafeTopAppBar
 import project.odycafe.navigasi.DestinasiNavigasi
 
 object ItemEditMenuDestination : DestinasiNavigasi {
@@ -24,5 +30,28 @@ fun ItemEditMenuScreen(
     modifier: Modifier = Modifier,
     viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ) {
+    val coroutineScope = rememberCoroutineScope()
 
+    Scaffold(
+        topBar = {
+            CafeTopAppBar(
+                title = stringResource(ItemEditMenuDestination.titleRes),
+                canNavigateBack = true,
+                navigateUp = onNavigateUp
+            )
+        },
+        modifier = modifier
+    ) { innerPadding ->
+        EntryMenuBody(
+            uiStateMenu = viewModel.menuUiState,
+            onMenuValueChange = viewModel::updateUiStateMenu,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.updateMenu()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
 }
