0d1096bac388659d4935d6b321053f82c5c96bcc
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2024-01-11 07:19:18 +0700

    Add HalamanEntryMenu.kt with a Object DestinasiMenuEntry

---
+++ app/src/main/java/project/odycafe/ui/theme/halaman/HalamanEntryMenu.kt
@@ -0,0 +1,7 @@
+import project.odycafe.R
+import project.odycafe.navigasi.DestinasiNavigasi
+
+object DestinasiMenuEntry: DestinasiNavigasi {
+    override val route = "item_entry_menu"
+    override val titleRes = R.string.title_entry_menu
+}
