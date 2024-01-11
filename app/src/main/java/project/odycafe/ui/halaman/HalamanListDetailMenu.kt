c2d3e68211d89ed07eae307646de8d56f1922503
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2024-01-11 18:40:25 +0700

    Add 3 Function For DetailsMenuListScreen

--- app/src/main/java/project/odycafe/ui/halaman/HalamanListDetailMenu.kt
+++ app/src/main/java/project/odycafe/ui/halaman/HalamanListDetailMenu.kt
@@ -1,16 +1,43 @@
 package project.odycafe.ui.halaman
 
 import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import project.odycafe.R
+import project.odycafe.data.Menu
+import project.odycafe.model.DetailsViewModel
+import project.odycafe.model.ItemDetailsMenuUiState
+import project.odycafe.model.PenyediaViewModel
+import project.odycafe.model.toMenu
+import project.odycafe.navigasi.CafeTopAppBar
 import project.odycafe.navigasi.DestinasiNavigasi
 
 object DetailsMenuListDestination : DestinasiNavigasi {
@@ -18,6 +45,115 @@ object DetailsMenuListDestination : DestinasiNavigasi {
     override val titleRes = R.string.title_detail_menu
     const val detailIdArg = "itemId"
     val routeWithArgs = "$route/{$detailIdArg}"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailsMenuListScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: DetailsViewModel = viewModel(factory = PenyediaViewModel.Factory) /** -> Penyedia View Model */
+){
+    val uiState = viewModel.uiStateMenu.collectAsState()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CafeTopAppBar(
+                title = stringResource(DetailsMenuDestination.titleRes),
+                canNavigateBack = true,
+                navigateUp = navigateBack,
+                modifier = Modifier.alpha(0.5f),
+            )
+        },
+    ) { innerPadding ->
+        BoxWithConstraints(modifier = Modifier.fillMaxSize()) {
+            Image(
+                painter = painterResource(id = R.drawable.esteh),
+                contentDescription = null,
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.FillBounds,
+            )
+
+            ItemMenuListDetailBody(
+                itemDetailsMenuUiState = uiState.value,
+                modifier = Modifier
+                    .padding(innerPadding)
+                    .verticalScroll(rememberScrollState()),
+            )
+        }
+    }
+}
+
+@Composable
+private fun ItemMenuListDetailBody(
+    itemDetailsMenuUiState: ItemDetailsMenuUiState,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.padding(dimensionResource(id = R.dimen.padding_medium)),
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
+    ) {
+        ItemMenuListDetails(
+            menu = itemDetailsMenuUiState.detailMenu.toMenu(),
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+}
+
+@Composable
+fun ItemMenuListDetails(
+    menu : Menu, modifier: Modifier = Modifier
+){
+    Card(
+        modifier = modifier
+            .padding(top = 165.dp)
+            .size(width = 350.dp, height = 275.dp)
+            .alpha(0.8f),
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
+    ) {
+        Column(
+            modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_Large)),
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small))
+        ) {
+            ItemMenuListDetailsRow(
+                labelResID = R.string.idmenu1,
+                itemDetail = menu.idmenu.toString(),
+                modifier = Modifier.padding(
+                    horizontal = dimensionResource(id = R.dimen.padding_medium)
+                )
+            )
+            ItemMenuListDetailsRow(
+                labelResID = R.string.menu1,
+                itemDetail = menu.menu,
+                modifier = Modifier.padding(
+                    horizontal = dimensionResource(id = R.dimen.padding_medium)
+                )
+            )
+            ItemMenuListDetailsRow(
+                labelResID = R.string.harga1,
+                itemDetail = menu.harga,
+                modifier = Modifier.padding(
+                    horizontal = dimensionResource(id = R.dimen.padding_medium)
+                )
+            )
+            ItemMenuListDetailsRow(
+                labelResID = R.string.ketersediaan1,
+                itemDetail = menu.ketersediaan,
+                modifier = Modifier.padding(
+                    horizontal = dimensionResource(id = R.dimen.padding_medium)
+                )
+            )
+            ItemMenuListDetailsRow(
+                labelResID = R.string.kategori1,
+                itemDetail = menu.kategori,
+                modifier = Modifier.padding(
+                    horizontal = dimensionResource(id = R.dimen.padding_medium)
+                )
+            )
+        }
+    }
 }
 
 @Composable
