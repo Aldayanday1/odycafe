e405017ff70136836f6338ee299d24d73d78eeff
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2024-01-11 18:55:11 +0700

    Add 3 Function For DetailsPesananListScreen

--- app/src/main/java/project/odycafe/ui/halaman/HalamanListDetailPesanan.kt
+++ app/src/main/java/project/odycafe/ui/halaman/HalamanListDetailPesanan.kt
@@ -1,16 +1,43 @@
 package project.odycafe.ui.halaman
 
 import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import project.odycafe.R
+import project.odycafe.data.Pesanan
+import project.odycafe.model.DetailsViewModel
+import project.odycafe.model.ItemDetailsPesananUiState
+import project.odycafe.model.PenyediaViewModel
+import project.odycafe.model.toPesanan
+import project.odycafe.navigasi.CafeTopAppBar
 import project.odycafe.navigasi.DestinasiNavigasi
 
 object DetailsPesananListDestination : DestinasiNavigasi {
@@ -20,6 +47,122 @@ object DetailsPesananListDestination : DestinasiNavigasi {
     val routeWithArgs = "$route/{$detailIdArg}"
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailsPesananListScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: DetailsViewModel = viewModel(factory = PenyediaViewModel.Factory) /** -> Penyedia View Model */
+){
+    val uiState = viewModel.uiStatePesanan.collectAsState()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CafeTopAppBar(
+                title = stringResource(DetailsPesananDestination.titleRes),
+                canNavigateBack = true,
+                navigateUp = navigateBack,
+                modifier = Modifier.alpha(0.5f),
+            )
+        },
+    ) { innerPadding ->
+        BoxWithConstraints(modifier = Modifier.fillMaxSize()) {
+            Image(
+                painter = painterResource(id = R.drawable.esteh),
+                contentDescription = null,
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.FillBounds,
+            )
+
+            ItemPesananListDetailBody(
+                itemDetailsPesananUiState = uiState.value,
+                modifier = Modifier
+                    .padding(innerPadding)
+                    .verticalScroll(rememberScrollState()),
+            )
+        }
+    }
+}
+
+@Composable
+private fun ItemPesananListDetailBody(
+    itemDetailsPesananUiState: ItemDetailsPesananUiState,
+    modifier: Modifier = Modifier
+){
+    Column(
+        modifier = modifier.padding(dimensionResource(id = R.dimen.padding_medium)),
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
+    ) {
+        ItemPesananListDetails(
+            pesanan = itemDetailsPesananUiState.detailPesanan.toPesanan(),
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+}
+
+@Composable
+fun ItemPesananListDetails(
+    pesanan : Pesanan, modifier: Modifier = Modifier
+){
+    Card(
+        modifier = modifier
+            .padding(top = 145.dp)
+            .size(width = 350.dp, height = 315.dp)
+            .alpha(0.8f),
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
+    ) {
+        Column(
+            modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_Large)),
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small))
+        ) {
+            ItemPesananListDetailsRow(
+                labelResID = R.string.idpesanan1,
+                itemDetail = pesanan.idpesanan.toString(),
+                modifier = Modifier.padding(
+                    horizontal = dimensionResource(id = R.dimen.padding_medium)
+                )
+            )
+            ItemPesananListDetailsRow(
+                labelResID = R.string.nama1,
+                itemDetail = pesanan.nama,
+                modifier = Modifier.padding(
+                    horizontal = dimensionResource(id = R.dimen.padding_medium)
+                )
+            )
+            ItemPesananListDetailsRow(
+                labelResID = R.string.idmenu1,
+                itemDetail = pesanan.idMenuForeignKey.toString(),
+                modifier = Modifier.padding(
+                    horizontal = dimensionResource(id = R.dimen.padding_medium)
+                )
+            )
+            ItemPesananListDetailsRow(
+                labelResID = R.string.detail1,
+                itemDetail = pesanan.detail,
+                modifier = Modifier.padding(
+                    horizontal = dimensionResource(id = R.dimen.padding_medium)
+                )
+            )
+            ItemPesananListDetailsRow(
+                labelResID = R.string.metode1,
+                itemDetail = pesanan.metode,
+                modifier = Modifier.padding(
+                    horizontal = dimensionResource(id = R.dimen.padding_medium)
+                )
+            )
+            ItemPesananListDetailsRow(
+                labelResID = R.string.tanggal1,
+                itemDetail = pesanan.tanggal,
+                modifier = Modifier.padding(
+                    horizontal = dimensionResource(id = R.dimen.padding_medium)
+                )
+            )
+        }
+
+    }
+}
 @Composable
 fun ItemPesananListDetailsRow(
     @StringRes labelResID: Int, itemDetail: String, modifier: Modifier = Modifier
