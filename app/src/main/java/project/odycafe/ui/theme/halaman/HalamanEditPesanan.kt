6d13fd2757b0183ce87aba9447f4240dc5af42c3
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2024-01-11 07:11:00 +0700

    Add Instance of coroutineScope and menuItems in Fun ItemEditPesananScreen

--- app/src/main/java/project/odycafe/ui/theme/halaman/HalamanEditPesanan.kt
+++ app/src/main/java/project/odycafe/ui/theme/halaman/HalamanEditPesanan.kt
@@ -1,12 +1,20 @@
 package project.odycafe.ui.theme.halaman
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
+import kotlinx.coroutines.launch
 import project.odycafe.R
 import project.odycafe.model.EditViewModel
 import project.odycafe.model.PenyediaViewModel
+import project.odycafe.navigasi.CafeTopAppBar
 import project.odycafe.navigasi.DestinasiNavigasi
 
 object ItemEditPesananDestination : DestinasiNavigasi {
@@ -24,5 +32,35 @@ fun ItemEditPesananScreen(
     modifier: Modifier = Modifier,
     viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ) {
+    val coroutineScope = rememberCoroutineScope()
 
+    // Mengambil menuItems dari viewModel
+    val menuItems by viewModel.menuItems.collectAsState()
+
+    Scaffold(
+        topBar = {
+            CafeTopAppBar(
+                title = stringResource(ItemEditPesananDestination.titleRes),
+                canNavigateBack = true,
+                navigateUp = onNavigateUp
+            )
+        },
+        modifier = modifier
+    ) { innerPadding ->
+        EntryPesananBody(
+            uiStatePesanan = viewModel.pesananUiState,
+            onPesananValueChange = { detailPesanan ->
+                viewModel.updateUiStatePesanan(detailPesanan, menuItems) // Pastikan hanya satu parameter DetailPesanan yang dikirimkan
+            },
+            onSaveClick = {
+                coroutineScope.launch {
+                    // Memastikan bahwa menuItems diteruskan ke viewModel.updatePesanan()
+                    viewModel.updatePesanan(menuItems)
+                    navigateBack()
+                }
+            },
+            menuItems = menuItems, // Menggunakan menuItems sebagai argumen di EntryPesananBody
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
 }
